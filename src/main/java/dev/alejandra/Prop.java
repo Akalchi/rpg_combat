0baaef866f0a5e37bed7f16d7288e8e19cc3d9f2
Akalchi <alejandrafdez1699@gmail.com>
2025-03-05 19:04:43 +0100

    "FEAT [Prop] implemented to fix failing tests"

---
+++ src/main/java/dev/alejandra/Prop.java
@@ -0,0 +1,19 @@
+package dev.alejandra;
+
+public class Prop {
+
+    private int health;
+    
+    public Prop(int initialHealth) {
+        this.health = initialHealth;
+    }
+    
+    public int getHealth() {
+        return health;
+    }
+    
+    public boolean isDestroyed() {
+        return false;
+    }
+
+}
