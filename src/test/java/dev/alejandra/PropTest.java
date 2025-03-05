27f6ff15ce99e161f2b5ebc78b89e55a44fc383b
Akalchi <alejandrafdez1699@gmail.com>
2025-03-05 19:03:18 +0100

    "TEST [PropInitialization] failed"

---
+++ src/test/java/dev/alejandra/PropTest.java
@@ -0,0 +1,20 @@
+package dev.alejandra;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class PropTest {
+
+    @Test
+    public void testPropInitialization() {
+        
+        Prop tree = new Prop(2000);
+        
+        assertEquals(2000, tree.getHealth());
+        assertFalse(tree.isDestroyed());
+    }
+}    
+
